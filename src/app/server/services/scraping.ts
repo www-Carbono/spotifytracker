96b68d230ab8f6a569ce6a5d77da5bb0312cd618
Antonio González <dev.antonio.gonzalez.parla@gmail.com>
2024-04-26 14:17:35 +0200

    update

--- src/app/server/services/scraping.ts
+++ src/app/server/services/scraping.ts
@@ -1,5 +1,5 @@
 'use server'
-import Spotify from 'spotify-finder'
+// import Spotify from 'spotify-finder'
 const data = async (): Promise<any> => {
   const first = await fetch(
     'https://api-partner.spotify.com/pathfinder/v1/query?operationName=getAlbum&variables=%7B%22uri%22%3A%22spotify%3Aalbum%3A2VZ9rmzFLZbsMMrmrJF8ur%22%2C%22locale%22%3A%22intl-es%22%2C%22offset%22%3A0%2C%22limit%22%3A50%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%22469874edcad37b7a379d4f22f0083a49ea3d6ae097916120d9bbe3e36ca79e9d%22%7D%7D',
@@ -36,12 +36,12 @@ const data = async (): Promise<any> => {
 }
 
 const scrapeSong = (): void => {
-  const client = new Spotify({
-    consumer: {
-      key: '47c598b772a34e67b66a53f78927cdbc', // from v2.1.0 is required
-      secret: 'ab8ebcd80825414b96bcd5001682db6c' // from v2.1.0 is required
-    }
-  })
+  //   const client = new Spotify({
+  //     consumer: {
+  //       key: '47c598b772a34e67b66a53f78927cdbc', // from v2.1.0 is required
+  //       secret: 'ab8ebcd80825414b96bcd5001682db6c' // from v2.1.0 is required
+  //     }
+  //   })
 
   //   const params = {
   //     q: 'Rompeolas Martin Urrutia', // required
