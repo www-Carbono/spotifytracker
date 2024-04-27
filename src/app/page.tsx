ebdd7a0151dae9eb53f83dff06e383266ed8b05d
Antonio González <dev.antonio.gonzalez.parla@gmail.com>
2024-04-27 19:06:51 +0200

    .env file fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { SearchSong } from './components/SearchSong'
+import saveToDatabase from './server/services/saveToDatabase'
 
 const Home = (): JSX.Element => {
+  useEffect(() => {
+    saveToDatabase()
+  }, [])
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <SearchSong />
--- src/app/server/services/saveToDatabase.ts
+++ src/app/server/services/saveToDatabase.ts
@@ -1,2 +1,12 @@
 'use server'
 // Guardar Nuevas Canciones en la Base de Datos
+import { createClient } from '@supabase/supabase-js'
+
+const saveToDatabase = (): void => {
+  console.log(process.env.SUPABASE_KEY)
+  //   const supabaseUrl = process.env.SUPABASE_URL
+  //   const supabaseKey = process.env.SUPABASE_KEY
+  //   const supabase = createClient(supabaseUrl, supabaseKey)
+}
+
+export default saveToDatabase
