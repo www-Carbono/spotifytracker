22d223ed8fa5db2c36ac2f8b5d337ef81f2e1af1
Antonio González <dev.antonio.gonzalez.parla@gmail.com>
2024-04-29 14:48:53 +0200

    fixed production

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 'use client'
 import React from 'react'
-import { SearchSong } from './../components/SearchSong'
 
 const Home = (): JSX.Element => {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <SearchSong />
+      <h1>Welcome</h1>
     </main>
   )
 }
