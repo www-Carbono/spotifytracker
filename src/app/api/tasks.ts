e38d49c224f368f5428c634e3dfbf7ec83414d6a
Antonio González <dev.antonio.gonzalez.parla@gmail.com>
2024-05-02 19:43:04 +0200

    tasks updated

--- src/app/api/tasks.ts
+++ src/app/api/tasks.ts
@@ -4,9 +4,11 @@ const handler = async (req: any, res: any): Promise<void> => {
   try {
     await isSpotifyUpdated()
     // task was successful, respond with 200
-    res.status(200).send('')
+    console.log('pasa por aqui')
+    res.status(200).send('results')
   } catch (err) {
     // task failed, respond with 500 so Mergent will retry
+    console.log('error')
     res.status(500).send({ error: err })
   }
 }
