6bbe2f06decd0c7b924d259a940c2954235e4a4a
Antonio González <dev.antonio.gonzalez.parla@gmail.com>
2024-05-02 20:22:15 +0200

    test

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-confusing-void-expression */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable @typescript-eslint/space-before-function-paren */
 import { isSpotifyUpdated } from '@/server/services/checkSpotifyUpdates'
@@ -7,6 +8,7 @@ import { type NextRequest, NextResponse } from 'next/server'
 
 export async function GET(request: NextRequest) {
   const data = await isSpotifyUpdated()
+  console.log(data)
   const greeting = 'Hello World!!'
   const json = {
     greeting
