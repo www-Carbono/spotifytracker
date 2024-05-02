6de37e0b3bf6e7ff3acac91b708e3b3251cadf8d
Antonio González <dev.antonio.gonzalez.parla@gmail.com>
2024-05-03 00:31:16 +0200

    testing

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -1,12 +1,19 @@
 /* eslint-disable @typescript-eslint/space-before-function-paren */
 import { isSpotifyUpdated } from '@/server/services/checkSpotifyUpdates'
 
+import { createClient } from '@supabase/supabase-js'
+
 // api > hello > route.ts
 
 import { NextResponse } from 'next/server'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? ''
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY ?? ''
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 export async function GET(): Promise<any> {
   console.log('Funciona Correctamente')
+  const { data, error } = await supabase.rpc('testing')
+
   const { songViews, artistFollowers, artistMonthlyListeners } =
     await isSpotifyUpdated()
 
@@ -17,7 +24,4 @@ export async function GET(): Promise<any> {
     artistFollowers,
     artistMonthlyListeners
   })
-  // return NextResponse.json({
-  //   ok: 'ok'
-  // })
 }
