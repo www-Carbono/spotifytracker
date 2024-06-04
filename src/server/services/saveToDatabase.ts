612be633fd0ed5526b272b81d30b307ef7aa471f
Antonio González <dev.antonio.gonzalez.parla@gmail.com>
2024-06-04 10:57:41 +0200

    data fixed

--- src/server/services/saveToDatabase.ts
+++ src/server/services/saveToDatabase.ts
@@ -187,7 +187,7 @@ const tryUpdateAgain = async (
   type: string,
   notUpdated: any[]
 ): Promise<any> => {
-  const CONTADOR_VALUE = 10
+  const CONTADOR_VALUE = 25
   while (notUpdated.length > 0) {
     console.log('INICIO BUCLE')
     for (const elementId of notUpdated) {
@@ -206,7 +206,7 @@ const tryUpdateAgain = async (
             // Tengo que comparar oyentes/views/followers del element(elemento de la base de datos) y el dataView (data actual de escuchas oyentes etc)
             const lastSavedData = Object.values(data[0].monthlylisteners).pop()
             if (lastSavedData === dataView.followers) {
-              await new Promise((resolve) => setTimeout(resolve, 5000))
+              await new Promise((resolve) => setTimeout(resolve, 15000))
               contador++
               if (contador === CONTADOR_VALUE - 1) {
                 const nuevoArray = notUpdated.filter(
@@ -251,7 +251,7 @@ const tryUpdateAgain = async (
             // Tengo que comparar oyentes/views/followers del element(elemento de la base de datos) y el dataView (data actual de escuchas oyentes etc)
             const lastSavedData = Object.values(data[0].viewsTest).pop()
             if (lastSavedData === Number(dataView)) {
-              await new Promise((resolve) => setTimeout(resolve, 5000))
+              await new Promise((resolve) => setTimeout(resolve, 15000))
               contador++
               if (contador === CONTADOR_VALUE - 1) {
                 const nuevoArray = notUpdated.filter(
@@ -294,7 +294,7 @@ const tryUpdateAgain = async (
           } else if (type === 'listeners') {
             const lastSavedData = Object.values(data[0].monthlylisteners).pop()
             if (lastSavedData === dataView.monthlyListeners) {
-              await new Promise((resolve) => setTimeout(resolve, 5000))
+              await new Promise((resolve) => setTimeout(resolve, 15000))
               contador++
               if (contador === CONTADOR_VALUE - 1) {
                 const nuevoArray = notUpdated.filter(
